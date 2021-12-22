c4c26fd05458de98cb618b626705f5e7f68e8bf3
Mykhaylo <michele.boychuk@gmail.com>
2021-12-22 10:26:13 +0100

    fix failed tests

--- dspace-api/src/test/java/org/dspace/content/authority/EntityTypeAuthorityFilterTest.java
+++ dspace-api/src/test/java/org/dspace/content/authority/EntityTypeAuthorityFilterTest.java
@@ -34,7 +34,7 @@ public class EntityTypeAuthorityFilterTest {
 
         // Test set up ItemAuthority is EntityOrg - Organization 1
         Mockito.when(authority.getPluginInstanceName()).thenReturn("Organization 1");
-        Mockito.when(authority.getLinkedEntityType()[0]).thenReturn("EntityOrg");
+        Mockito.when(authority.getLinkedEntityType()).thenReturn(new String[] {"EntityOrg"});
 
         List<String> queries = entityTypeAuthorityFilter.getFilterQueries(authority, "EntityOrg");
 
@@ -52,7 +52,7 @@ public class EntityTypeAuthorityFilterTest {
 
         // Test set up ItemAuthority is EntityAnother - Organization 2
         Mockito.when(authority.getPluginInstanceName()).thenReturn("Organization 2");
-        Mockito.when(authority.getLinkedEntityType()[0]).thenReturn("EntityAnother");
+        Mockito.when(authority.getLinkedEntityType()).thenReturn(new String[] {"EntityAnother"});
 
         List<String> queries = entityTypeAuthorityFilter.getFilterQueries(authority, "EntityAnother");
 
@@ -71,7 +71,7 @@ public class EntityTypeAuthorityFilterTest {
 
         // Test set up ItemAuthority is EntityOrg - Organization 1
         Mockito.when(authority.getPluginInstanceName()).thenReturn("Organization 1");
-        Mockito.when(authority.getLinkedEntityType()[0]).thenReturn("EntityOrg");
+        Mockito.when(authority.getLinkedEntityType()).thenReturn(new String[] {"EntityOrg"});
 
         List<String> queries = entityTypeAuthorityFilter.getFilterQueries(authority, "EntityOrg");
 
@@ -91,7 +91,7 @@ public class EntityTypeAuthorityFilterTest {
 
         // Test set up ItemAuthority is EntityAnother - Organization 1
         Mockito.when(authority.getPluginInstanceName()).thenReturn("Organization 1");
-        Mockito.when(authority.getLinkedEntityType()[0]).thenReturn("EntityAnother");
+        Mockito.when(authority.getLinkedEntityType()).thenReturn(new String[] {"EntityAnother"});
 
         List<String> queries = entityTypeAuthorityFilter.getFilterQueries(authority, "EntityAnother");
 

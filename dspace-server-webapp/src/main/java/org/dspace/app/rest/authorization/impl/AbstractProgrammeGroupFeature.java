329dd29db6589228120e139d820181c9f6f592e7
Vincenzo Mecca <vincenzo.mecca@4science.com>
2023-03-17 19:36:07 +0100

    [CST-9218] ProgrammeFeatures now checks on original item

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/authorization/impl/AbstractProgrammeGroupFeature.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/authorization/impl/AbstractProgrammeGroupFeature.java
@@ -7,13 +7,20 @@
  */
 package org.dspace.app.rest.authorization.impl;
 
+import static org.dspace.project.util.ProjectConstants.MD_UNIQUE_ID;
+
 import java.sql.SQLException;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.dspace.content.Item;
+import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
 import org.dspace.eperson.Group;
 import org.dspace.synsicris.programme.ProgrammeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -24,8 +31,13 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public abstract class AbstractProgrammeGroupFeature extends IsMemberOfProjectFeature {
 
+    private static final Logger logger = LoggerFactory.getLogger(AbstractProgrammeGroupFeature.class);
+
     @Autowired
     private ProgrammeService programmeService;
+
+    @Autowired
+    private ItemService itemService;
 
     @Override
     protected abstract String getRoleName();
@@ -34,7 +46,7 @@ public abstract class AbstractProgrammeGroupFeature extends IsMemberOfProjectFea
     protected Group getGroupFromRelatedItem(Context context, Item relatedItem, String roleName) throws SQLException {
         return getGroupFromItem(
             context,
-            projectConsumerService.getProjectItemByRelatedItem(context, relatedItem),
+            projectConsumerService.getProjectItemByRelatedItem(context, getOriginalItem(context, relatedItem)),
             roleName
         );
     }
@@ -46,11 +58,31 @@ public abstract class AbstractProgrammeGroupFeature extends IsMemberOfProjectFea
         }
         return programmeService.getProgrammeGroupByRole(
             context,
-            Optional.ofNullable(programmeService.getProgrammeByProgrammeRelation(context, projectItem))
+            Optional.ofNullable(
+                programmeService.getProgrammeByProgrammeRelation(context, getOriginalItem(context, projectItem))
+                )
                 .map(Item::getID)
                 .orElse(null),
             roleName
         );
+    }
+
+    protected Item getOriginalItem(Context context, Item relatedItem) {
+        return Optional.ofNullable(
+            this.itemService.getMetadataFirstValue(relatedItem, MD_UNIQUE_ID, Item.ANY)
+        )
+        .map(uniqueId -> uniqueId.split("_")[0])
+        .filter(Objects::nonNull)
+        .map(uuid -> {
+            Item foundItem = null;
+            try {
+                foundItem = this.itemService.find(context, UUID.fromString(uuid));
+            } catch (SQLException e) {
+                logger.error("Error while retrieving the original item from the unique_id with uuid: " + uuid, e);
+            }
+            return foundItem;
+        })
+        .orElse(relatedItem);
     }
 
 }

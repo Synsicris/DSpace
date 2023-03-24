9505478b78ec26f723b40de921a5339ed289604b
Vincenzo Mecca <vincenzo.mecca@4science.com>
2023-03-24 17:43:17 +0100

    [DURACOM-125][#8736][DSC-998][CST-9431] Preserved order of modified MetadataValues
    feat:
      - Introduced new Class with static methods usable to order
    `MetadataValue` lists;
      - Introduced ITs also for `PatchMetadata` replace operations;
      - Introduced new method `DspaceObject#getMetadataFieldId`.
    ref:
      - Replaced old sort method inside `ItemServiceImpl` with the new one

--- dspace-api/src/main/java/org/dspace/content/ItemServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/content/ItemServiceImpl.java
@@ -12,7 +12,6 @@ import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -1696,7 +1695,7 @@ prevent the generation of resource policy entry values with null dspace_object a
             fullMetadataValueList.addAll(relationshipMetadataService.getRelationshipMetadata(item, true));
             fullMetadataValueList.addAll(dbMetadataValues);
 
-            item.setCachedMetadata(sortMetadataValueList(fullMetadataValueList));
+            item.setCachedMetadata(MetadataValueComparators.sort(fullMetadataValueList));
         }
 
         log.debug("Called getMetadata for " + item.getID() + " based on cache");
@@ -1752,29 +1751,6 @@ prevent the generation of resource policy entry values with null dspace_object a
             //just move the metadata
             rr.setPlace(place);
         }
-    }
-
-    /**
-     * This method will sort the List of MetadataValue objects based on the MetadataSchema, MetadataField Element,
-     * MetadataField Qualifier and MetadataField Place in that order.
-     *
-     * @param listToReturn The list to be sorted
-     * @return The list sorted on those criteria
-     */
-    private List<MetadataValue> sortMetadataValueList(List<MetadataValue> listToReturn) {
-        Comparator<MetadataValue> comparator = Comparator.comparing(
-            metadataValue -> metadataValue.getMetadataField().getMetadataSchema().getName(),
-            Comparator.nullsFirst(Comparator.naturalOrder()));
-        comparator = comparator.thenComparing(metadataValue -> metadataValue.getMetadataField().getElement(),
-            Comparator.nullsFirst(Comparator.naturalOrder()));
-        comparator = comparator.thenComparing(metadataValue -> metadataValue.getMetadataField().getQualifier(),
-            Comparator.nullsFirst(Comparator.naturalOrder()));
-        comparator = comparator.thenComparing(metadataValue -> metadataValue.getPlace(),
-            Comparator.nullsFirst(Comparator.naturalOrder()));
-
-        Stream<MetadataValue> metadataValueStream = listToReturn.stream().sorted(comparator);
-        listToReturn = metadataValueStream.collect(Collectors.toList());
-        return listToReturn;
     }
 
     @Override

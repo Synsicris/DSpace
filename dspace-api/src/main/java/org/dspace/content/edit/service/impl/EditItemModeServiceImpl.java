1445ae4352765f4b7fd8aff1d975376a25425afd
Mykhaylo <michele.boychuk@gmail.com>
2023-06-26 16:40:43 +0200

    [CST-10123] removed edit mode validation

--- dspace-api/src/main/java/org/dspace/content/edit/service/impl/EditItemModeServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/content/edit/service/impl/EditItemModeServiceImpl.java
@@ -14,14 +14,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import javax.annotation.PostConstruct;
 
 import org.dspace.app.util.SubmissionConfigReader;
-import org.dspace.app.util.SubmissionConfigReaderException;
 import org.dspace.content.Item;
 import org.dspace.content.edit.EditItemMode;
 import org.dspace.content.edit.service.EditItemModeService;
-import org.dspace.content.edit.service.EditItemModeValidator;
 import org.dspace.content.security.service.CrisSecurityService;
 import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
@@ -39,22 +36,12 @@ public class EditItemModeServiceImpl implements EditItemModeService {
 
     @Autowired
     private ItemService itemService;
-
     @Autowired
     private CrisSecurityService crisSecurityService;
-
-    @Autowired
-    private EditItemModeValidator editItemModeValidator;
 
     private SubmissionConfigReader submissionConfigReader;
 
     private Map<String, List<EditItemMode>> editModesMap;
-
-    @PostConstruct
-    private void setup() throws SubmissionConfigReaderException {
-        editItemModeValidator.validate(editModesMap);
-        submissionConfigReader = new SubmissionConfigReader();
-    }
 
     @Override
     public List<EditItemMode> findModes(Context context, Item item) throws SQLException {
